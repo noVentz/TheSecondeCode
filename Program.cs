8fc882352ce235ddc183ea5c00f3ce018735a235
noVentz <103551295+noVentz@users.noreply.github.com>
2022-04-13 07:11:48 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FirstApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            bool logic;
+            char symbol;
+            long year;
+            decimal finance;
+            byte bit;
+            sbyte bitSymbol;
+            long numderLong;
+            ushort smalNumber;
+            short smalNumberSymbol;
+        }
+    }
+}
